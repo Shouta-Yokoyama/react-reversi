c9a012a7401d926c7fe5e6c611751bdca4bf00c6
Shouta-Yokoyama <zeru1357@gmail.com>
2024-10-13 06:14:00 +0900

    新規実装

--- client/src/__tests__/Grid.test.js
+++ client/src/__tests__/Grid.test.js
@@ -6,7 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const react_1 = require("@testing-library/react");
 const react_2 = __importDefault(require("react"));
 const Grid_1 = __importDefault(require("../components/Grid"));
+const Square_1 = __importDefault(require("../components/Square"));
 const BoardContext_1 = require("../contexts/BoardContext");
+jest.mock("../components/Square");
 const mockContext = {
     boardState: Array(8).fill(Array(8).fill(0)),
     handleClick: jest.fn(),
@@ -15,3 +17,22 @@ const renderGrid = () => {
     return (0, react_1.render)(react_2.default.createElement(BoardContext_1.BoardContext.Provider, { value: mockContext },
         react_2.default.createElement(Grid_1.default, null)));
 };
+describe("Grid component", () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+    it("renders a grid", () => {
+        renderGrid();
+        expect(react_1.screen.getByRole("table")).toBeInTheDocument();
+    });
+    it("grid has a 8 rows", () => {
+        renderGrid();
+        const rows = react_1.screen.getAllByRole("row");
+        expect(rows).toHaveLength(8);
+    });
+    it("grid has a 64 squares", () => {
+        renderGrid();
+        const square = Square_1.default;
+        expect(square.mock.calls).toHaveLength(64);
+    });
+});
--- client/src/__tests__/Grid.test.tsx
+++ client/src/__tests__/Grid.test.tsx
@@ -1,17 +1,45 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import React from "react";
 import Grid from "../components/Grid";
+import Square from "../components/Square";
 import { BoardContext } from "../contexts/BoardContext";
+
+jest.mock("../components/Square");
 
 const mockContext = {
   boardState: Array(8).fill(Array(8).fill(0)),
   handleClick: jest.fn(),
 };
 
-const renderGrid = ()=> {
-    return render(
-        <BoardContext.Provider value={mockContext}>
-            <Grid />
-        </BoardContext.Provider>
-    );
+const renderGrid = () => {
+  return render(
+    <BoardContext.Provider value={mockContext}>
+      <Grid />
+    </BoardContext.Provider>
+  );
 };
+
+describe("Grid component", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("renders a grid", ()=>{
+    renderGrid();
+    expect(screen.getByRole("table")).toBeInTheDocument();
+  });
+
+  it("grid has a 8 rows", ()=> {
+    renderGrid();
+    const rows = screen.getAllByRole("row");
+    expect(rows).toHaveLength(8);
+  });
+
+
+  it("grid has a 64 squares", ()=> {
+    renderGrid();
+    const square = Square as jest.Mock;
+    expect(square.mock.calls).toHaveLength(64);
+  });
+
+});
